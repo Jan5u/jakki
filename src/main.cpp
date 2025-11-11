161812ee52215af39921b102aa075396795ee747
jan5u <janhenrik02@hotmail.fi>
2025-11-11 11:16:10 +0200

    Added application icon and resource files

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/images/icon.ico"));
     MainWindow w;
     w.show();
     return app.exec(); 
