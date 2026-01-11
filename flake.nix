5a267a8e76a594c19395e4b7df96860b2e12c066
Valtteri Verronen <valtteri@verronen.dev>
2026-01-11 14:17:13 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            qt6.qtwayland
+            ninja
+            cmake
+            pkg-config
+            openssl.dev
+            libopus.dev
+            pipewire.dev
+            ffmpeg-full
+            nv-codec-headers-12
+            nlohmann_json
+            pipewire
+            libnotify
+            shaderc
+            gdb
+         ];
+        };
+      }
+    );
+}
