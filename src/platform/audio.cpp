2a6fc987f7a96c1f7bc308589856dbdbc12b3917
jan5u <janhenrik02@hotmail.fi>
2026-05-02 17:50:17 +0300

    Audio playback and capture callbacks

--- src/platform/audio.cpp
+++ src/platform/audio.cpp
@@ -2,6 +2,24 @@
 
 #include <stdio.h>
 
+static void SDLCALL playbackCallback(void *userdata, SDL_AudioStream *astream, int additional_amount, int total_amount) {
+    additional_amount /= sizeof(float);
+    while (additional_amount > 0) {
+        float samples[128];
+        const int total = SDL_min(additional_amount, SDL_arraysize(samples));
+        int i;
+
+        // process audio here
+
+        SDL_PutAudioStreamData(astream, samples, total * sizeof(float));
+        additional_amount -= total;
+    }
+}
+
+static void SDLCALL captureCallback(void *userdata, SDL_AudioStream *astream, int additional_amount, int total_amount) {
+    // SDL_GetAudioStreamData();
+}
+
 int Audio::init() {
 
     // const SDL_AudioSpec spec = {
@@ -10,35 +28,12 @@ int Audio::init() {
     //     .freq = 48000
     // };
 
-    playback_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, nullptr, nullptr, nullptr);
-    capture_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_RECORDING, nullptr, nullptr, nullptr);
-
-    return 0;
-}
-
-void Audio::startPlaybackStream() {
+    playback_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, nullptr, playbackCallback, nullptr);
+    capture_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_RECORDING, nullptr, captureCallback, nullptr);
     SDL_ResumeAudioStreamDevice(playback_stream);
-
-    playback_thread = std::jthread([this] {
-        while (true) {
-            // SDL_PutAudioStreamData();
-            // SDL_PutAudioStreamDataNoCopy();
-        }
-        SDL_DestroyAudioStream(playback_stream);
-    });
-
-    SDL_DestroyAudioStream(playback_stream);
-}
-
-void Audio::startCaptureStream() {
     SDL_ResumeAudioStreamDevice(capture_stream);
 
-    capture_thread = std::jthread([this] {
-        while (true) {
-            // SDL_GetAudioStreamData();
-        }
-        SDL_DestroyAudioStream(capture_stream);
-    });
+    return 0;
 }
 
 using DeviceGetter = SDL_AudioDeviceID *(*)(int *);
--- src/platform/audio.hpp
+++ src/platform/audio.hpp
@@ -1,20 +1,17 @@
-#include <thread>
-#include <vector>
+#pragma once
+
 #include <string>
+#include <vector>
 
 #include <SDL3/SDL_audio.h>
 
 class Audio {
   public:
     int init();
-    void startPlaybackStream();
-    void startCaptureStream();
     std::vector<std::string> getPlaybackDevices();
     std::vector<std::string> getCaptureDevices();
 
   private:
     SDL_AudioStream *playback_stream = nullptr;
     SDL_AudioStream *capture_stream = nullptr;
-    std::jthread playback_thread;
-    std::jthread capture_thread;
 };
