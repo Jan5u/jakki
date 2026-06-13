350c996b9daacd407f6160ca8e6fbbd17512892d
jan5u <janhenrik02@hotmail.fi>
2026-06-13 19:36:42 +0300

    Added sdl3 renderer with imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,143 @@
-#include "gui.hpp"
-#include <QApplication>
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    app.setWindowIcon(QIcon(":/images/icon.ico"));
-    MainWindow w;
-    w.show();
-    return app.exec(); 
+
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
+#include <memory>
+#include <stdio.h>
+
+#include <SDL3/SDL.h>
+
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+
+struct App {
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+};
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+    auto app = std::make_unique<App>();
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
+        printf("Error: SDL_Init(): %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    SDL_WindowFlags window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+    app->window = SDL_CreateWindow("Dear ImGui SDL3+SDL_Renderer example", (int)(1280 * main_scale), (int)(800 * main_scale), window_flags);
+    if (app->window == nullptr) {
+        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    app->renderer = SDL_CreateRenderer(app->window, nullptr);
+    SDL_SetRenderVSync(app->renderer, 1);
+    if (app->renderer == nullptr) {
+        SDL_Log("Error: SDL_CreateRenderer(): %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetWindowPosition(app->window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(app->window);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    ImGui::StyleColorsDark();
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale);
+    style.FontScaleDpi = main_scale;
+    ImGui_ImplSDL3_InitForSDLRenderer(app->window, app->renderer);
+    ImGui_ImplSDLRenderer3_Init(app->renderer);
+
+    *appstate = app.release();
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    auto app = static_cast<App *>(appstate);
+
+    if (SDL_GetWindowFlags(app->window) & SDL_WINDOW_MINIMIZED) {
+        SDL_WaitEvent(nullptr);
+        return SDL_APP_CONTINUE;
+    }
+
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+    ImGuiIO &io = ImGui::GetIO();
+
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");
+
+        ImGui::Text("This is some useful text.");
+        ImGui::Checkbox("Demo Window", &show_demo_window);
+        ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+        ImGui::ColorEdit3("clear color", (float *)&clear_color);
+
+        if (ImGui::Button("Button"))
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::End();
+    }
+
+    if (show_another_window) {
+        ImGui::Begin("Another Window", &show_another_window);
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me"))
+            show_another_window = false;
+        ImGui::End();
+    }
+
+    ImGui::Render();
+    SDL_SetRenderScale(app->renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+    SDL_SetRenderDrawColorFloat(app->renderer, clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+    SDL_RenderClear(app->renderer);
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), app->renderer);
+    SDL_RenderPresent(app->renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    auto app = static_cast<App *>(appstate);
+    ImGui_ImplSDL3_ProcessEvent(event);
+    if (event->type == SDL_EVENT_QUIT)
+        return SDL_APP_SUCCESS;
+    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event->window.windowID == SDL_GetWindowID(app->window))
+        return SDL_APP_SUCCESS;
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    auto app = static_cast<App *>(appstate);
+
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(app->renderer);
+    SDL_DestroyWindow(app->window);
+    SDL_Quit();
 }
